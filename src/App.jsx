e3412cb46c848d6e709e58ba519e92fad4d275c4
Rahat-Hridoy <rahathridoyd2x@gmail.com>
2025-09-04 23:46:17 +0600

    home page initialized

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Home from "./pages/home/Home";
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <Home />
       <div className="container m-auto text-3xl">hello</div>
     </>
   );
